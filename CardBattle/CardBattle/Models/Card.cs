2adb8af539bc99029e921bdc15fe5780e795345d
Jean-Noël Gourdol <jngourdol@stormancer.com>
2016-11-03 11:26:29 +0100

    créated model

---
+++ CardBattle/CardBattle/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardBattle.Models
+{
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Values Values { get; set; }
+    }
+}
---
+++ CardBattle/CardBattle/Models/Values.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardBattle.Models
+{
+    public enum Values
+    {
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jacks,
+        Queen,
+        King,
+        Ace
+    }
+}
