fa104641d07090c50bf8f87ab00238219a0a5000
Windywyll <mauricejrd@gmail.com>
2016-11-21 10:58:17 +0100

    added beginning of AI

---
+++ CardBattle/CardBattle/Sarkhan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CardBattle.Models;
+
+namespace CardBattle
+{
+    class Sarkhan : IPlayer
+    {
+        public string Author
+        {
+            get
+            {
+                return "Jordan";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Sarkhan Le Fou";
+            }
+        }
+
+        private int numberOfPlayer, myId;
+        private List<Card> hand;
+
+        public void Deal(IEnumerable<Card> cards)
+        {
+            hand = new List<Card>(cards);
+            hand.Sort();
+        }
+
+        public void Initialize(int playerCount)
+        {
+            numberOfPlayer = playerCount;
+        }
+
+        public int PlayCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveFoldResult(FoldResult result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
